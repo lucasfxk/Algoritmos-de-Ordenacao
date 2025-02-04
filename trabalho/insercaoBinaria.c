ddf92687cc504464a8f1b9a44adbeb9fd62297bc
Lucas Borges <lbcarmo08@gmail.com>
2025-02-04 15:03:10 -0300

    Update insercaoBinaria.c Lucas' version

--- trabalho/insercaoBinaria.c
+++ trabalho/insercaoBinaria.c
@@ -2,68 +2,28 @@
 #include <stdlib.h>
 #include "insercaoBinaria.h"
 
-void ordenarInsercaoBinaria(int n, int vet[], int *contador, int *trocas){
-    int i, j, troca,cont;
-    int posicao,valor;
+void ordenarInsercaoBinaria(int tamanhi, int *vetor, int *comparacoes, int *trocas){
+    int aux=0,esq=0,dir=0,meio=0;
 
-    for(i=0;i<n-1;i++){
-        j=i+1;
-        cont=0;
-        (*contador)++;
-        while(j<n){
-            (*contador)++;
-            if(vet[i]>vet[j]){
-            (*trocas)++;
-            troca=vet[j];
-            vet[j]=vet[i];
-            vet[i]=troca;
-
-            if(i>0 && vet[i-1]>vet[i]){
-                (*contador)++;
-                posicao=buscaBinaria(vet,0,i-1,vet[i],contador);
-                valor=vet[i];
-                while(i!=posicao){
-                    (*trocas)++;
-                    vet[i]=vet[i-1];
-                    i--;
-                    cont++;
-                }
-                (*trocas)++;
-                vet[posicao]=valor;
-                i=i+cont;
+    //Ordenação
+    for(int x=1;x<tamanho;x++){
+        aux = vetor[x];
+        esq = 0;
+        dir = x;
+        (*comparacoes)++;
+        while(esq<dir){
+            (*comparacoes)++;
+            meio = (esq+dir)/2;
+            if(vetor[meio]<=aux){
+                esq = meio+1;
+            }else{
+                dir = meio;
             }
-           }
-        break;
         }
-    }
-
-}
-
-
-int buscaBinaria(int vet[], int ini, int fim, int valor, int *contador){
-    int meio;
-    meio=(ini+fim)/2;
-    if(valor==vet[meio]){
-        (*contador)++;
-        return meio;
-    }
-    else if(valor>vet[meio]&&valor<=vet[meio+1]){
-        (*contador)=(*contador)+2;
-        return meio+1;
-    }
-    else if(valor<vet[meio]&&valor>=vet[meio-1]){
-        (*contador)=(*contador)+3;
-        return meio;
-    }
-    else if(valor<vet[meio]){
-        (*contador)=(*contador)+4;
-        buscaBinaria(vet,ini,meio,valor,contador);
-    }
-    else if(valor>vet[meio]){
-        (*contador)=(*contador)+5;
-        buscaBinaria(vet,meio,fim,valor,contador);
+        for(int y=x;y>esq;y--){
+            vetor[y] = vetor[y-1];
+            (*trocas)++;
+        }
+        vetor[dir] = aux;
     }
 }
-
-
-
